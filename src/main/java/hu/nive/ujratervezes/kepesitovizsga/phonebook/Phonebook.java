d85f38fe98ae3c7dde58b6c6315a717e564a65b9
developer <dudikpal@gmail.com>
2021-04-29 11:03:29 +0200

    PhoneBook done

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -1,4 +1,36 @@
 package hu.nive.ujratervezes.kepesitovizsga.phonebook;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null || output.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        String separator = "";
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry item: contacts.entrySet()) {
+            if (item.getKey() == null || item.getValue() == null) {
+                throw new IllegalArgumentException();
+            }
+            String name = item.getKey().toString();
+            String phoneNumber = item.getValue().toString();
+            sb.append(separator);
+            sb.append(name + ": " + phoneNumber);
+            separator = "\n";
+        }
+        //String file = "src\\main\\resources\\" + output;
+        String file = output;
+        Path path = Path.of(file);
+        try (BufferedWriter bw = Files.newBufferedWriter(path)){
+            bw.write(sb.toString());
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Cannot write file, ", ioe);
+        }
+    }
 }
